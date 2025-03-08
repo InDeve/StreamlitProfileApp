7a716c384526fb96a764aee2bb2f4c75bdccd82f
InDeve <saikenasukito@gmail.com>
2025-03-07 20:14:24 -0600

    app description edit

--- app.py
+++ app.py
@@ -45,7 +45,7 @@ pg = st.navigation(
 
 # ''' SHARED ON ALL pages '''
 st.logo("assets/heart.png")
-st.sidebar.text("Adapted by Calvin Y | Base dashboard from Sven: @CodingIsFun")
+st.sidebar.text("Adapted by Calvin Y | Base dashboard by Sven: @CodingIsFun")
 
 # ''' RUN NAVIGATION '''
 pg.run()
